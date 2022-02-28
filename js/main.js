504d273eb7eb4bc92c4de92bf765487a8c1e21a9
Fox <90596095+KojikiKiraFox@users.noreply.github.com>
2022-02-28 15:42:05 +0300

    feat: add new section benefits, lessons, questions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
+     const swiper = new Swiper('.swiper-benefits', {
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
 
+      const secondSwiper = new Swiper(".lessons-swiper", {
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },},
+          navigation: {
+            nextEl: '.lessons-swiper-button-next',
+            prevEl: '.lessons-swiper-button-prev',
+          },
+      });
+      
     new WOW().init();
 
     // Валидация формы
